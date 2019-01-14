1b0623dfc4090d10d9572155093da9fce4e8f085
Vinicius <vicius.souza@gmail.com>
2019-01-14 13:19:48 -0200

    Primeira publicação!

---
+++ TodoApi/Startup.cs
@@ -0,0 +1,82 @@
+﻿
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
+using System.Reflection;
+using System.IO;
+using System;
+
+namespace TodoApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddSwaggerGen(c =>
+           {
+               c.SwaggerDoc("v1", new Info{
+                   Version = "v1",
+                   Title = "ToDo API",
+                   Description = "A simple example ASP.NET Core Web API",
+                   TermsOfService = "None",
+                   Contact = new Contact
+                   {
+                       Name = "Shayne Boyer",
+                       Email = string.Empty,
+                       Url = "https://twitter.com/spboyer"
+                   },
+                   License = new License
+                   {
+                       Name = "Use under LICX",
+                       Url = "https://example.com/license"
+                   }
+               });
+               
+               // Set the comments path for the Swagger JSON and UI.
+               string xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+               string xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+               c.IncludeXmlComments(xmlPath);
+           });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Minha API V1");
+                c.RoutePrefix = string.Empty;
+            });
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
