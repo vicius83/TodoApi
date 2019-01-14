4857a0fc4669c50be39023a58acd1b8ef3e20304
Vinicius <vicius.souza@gmail.com>
2019-01-14 13:40:08 -0200

    novo ajuste

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -13,14 +13,18 @@ namespace TodoApi
 #pragma warning disable CS1591
     public class Program
     {
+        private static IConfiguration config;
+
         public static void Main(string[] args)
         {
+            IConfigurationRoot config = new ConfigurationBuilder().AddCommandLine(args).Build();
             CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseConfiguration(config);
     }
 #pragma warning restore CS1591
 }
